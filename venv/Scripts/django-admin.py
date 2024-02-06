1e8084c31d0774e9089041979791141ca44a1b5f
dhiraj73ray <raydj987abc@gmail.com>
2024-02-06 13:25:18 +0530

    Remove venv folder from repository

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!A:\GitHub\django-student-management-system-master\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
